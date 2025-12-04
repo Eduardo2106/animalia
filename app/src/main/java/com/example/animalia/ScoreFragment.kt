a2b39266feb460c0c2bc0b72915fbc1e6c2b759e
Eduardo2106 <eduardogg288@gmail.com>
2025-12-03 20:40:33 -0600

    2erCommit-VersionCorregida de cartas

--- app/src/main/java/com/example/animalia/ScoreFragment.kt
+++ app/src/main/java/com/example/animalia/ScoreFragment.kt
@@ -34,7 +34,7 @@ class ScoreFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup ?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_score, container, false)
